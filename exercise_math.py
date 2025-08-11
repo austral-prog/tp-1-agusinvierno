448726f6adf926633d21dc555268ea3a8dc1fa3e
agusinvierno <agustinainvierno@gmail.com>
2025-08-11 12:37:30 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,20 @@
 def math():
     a = 57
     b = 7
+
+    suma = a + b
+    print(suma)
+    resta = a - b
+    print(resta)
+    multiplicacion = a * b
+    print(multiplicacion)
+    promedio = (a + b)/2
+    print(promedio)
+    cociente_entero= a // b
+    print(cociente_entero)
+    resto_division_entera = a % b
+    print(resto_division_entera)
+    division = a / b
+    print(division)
+
+#math()
